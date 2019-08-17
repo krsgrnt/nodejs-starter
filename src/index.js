7406bbc197ecd07e371a084164049efe06e3c3b0
Kris Grint <krisgrint@gmail.com>
2019-08-17 20:32:25 +0100

    adding https

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import events from 'events';
+import https from 'https';
+import fs from 'fs';
+import 'dotenv/config';
 
 const app = express();
 app.use(cors());
-const port = 1337;
+const port = 443;
+const em = new events.EventEmitter();
+
+https
+    .createServer(
+        {
+            key: fs.readFileSync('./ssl/server.key'),
+            cert: fs.readFileSync('./ssl/server.cert'),
+        },
+        app
+    )
+    .listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+em.on('knock', data => {
+    console.log('Received the knock event: ' + data);
+});
+
+em.emit('knock', "who's there?");
+
+console.log(process.env.MY_DATABASE_PASSWORD);
